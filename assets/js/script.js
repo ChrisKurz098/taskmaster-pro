44f3347b29757f0509452f7eff77f266f5b45141
CKURZ <ckurz098@gmail.com>
2021-12-02 12:28:04 -0800

    added drag and drop and remove functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,81 @@
+
+
 var tasks = {};
+//make .list-group sortable
+$(".card .list-group").sortable({
+  //then connect with all .list-group
+  connectWith: $(".card .list-group"),
+  //set params
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+
+  //do something for each action. This happens ANY time the sortable item changes
+  activate: function(event) {
+    
+  },
+  deactivate: function(event) {
+   
+  },
+  over: function(event) {
+    
+  },
+  out: function(event) {
+    
+  },
+
+  update: function(event) {
+// array to store the task data in
+var tempArr = [];
+
+// loop over current set of children in sortable list
+$(this).children().each(function() {
+  var text = $(this)
+    .find("p")
+    .text()
+    .trim();
+
+  var date = $(this)
+    .find("span")
+    .text()
+    .trim();
+
+  // add task data to the temp array as an object
+  tempArr.push({
+    text: text,
+    date: date
+  });
+  
+});//end children.each
+// trim down list's ID to match object property
+var arrName = $(this)
+  .attr("id")
+  .replace("list-", "");
+
+// update array on tasks object and save
+tasks[arrName] = tempArr;
+saveTasks();
+
+console.log(tempArr);
+
+  } //update end
+}); //sortable end
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
+
 
 var createTask = function (taskText, taskDate, taskList) {
   // create elements that make up a task item
